85aa2f24c5f72dd93e336e1ea323bcea3401a4c4
Nacho <nacho@eventix.nl>
2022-04-16 23:01:13 +0300

    WIP Add Google Maps Integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+    const render = (status: Status) => {
+        return <h1>{status}</h1>;
+    };
+
+  return (
+      <Wrapper apiKey="AIzaSyCubd3LJdqByD_eySPXbuXcE_BkkTRHSpc" render={render}>
+        <Component {...pageProps} />
+      </Wrapper>
+  )
 }
 
-export default MyApp
+export default App
